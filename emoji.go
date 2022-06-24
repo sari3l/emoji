e8c86d6bc2414adcb1ffe63b67bc1f032b2d6ee6
sari3l <i4cu.sari@gmail.com>
2022-06-24 13:07:21 +0800

    add return check

--- emoji.go
+++ emoji.go
@@ -8,45 +8,50 @@ import (
 func emojiHook(fd int, p []byte) (n int, err error) {
 	pPtr := &p
 	pLen := len(p)
+	var waitForWriteBytes []byte
 	var emojiBytes []byte
 	lock := false
-	var lockBytes []byte
 	for i := 0; i < pLen; i++ {
 		if p[i] != 58 && !lock {
-			emojiBytes = append(emojiBytes, p[i])
+			waitForWriteBytes = append(waitForWriteBytes, p[i])
 			continue
 		}
 		if p[i] == 58 && !lock {
 			lock = true
-			lockBytes = append(lockBytes, p[i])
+			emojiBytes = append(emojiBytes, p[i])
 			continue
 		}
 		if p[i] == 58 && lock {
-			if len(lockBytes) == 1 {
-				emojiBytes = append(emojiBytes, lockBytes...)
+			if len(emojiBytes) == 1 {
+				waitForWriteBytes = append(waitForWriteBytes, emojiBytes...)
 				continue
 			}
 			lock = false
-			lockBytes = append(lockBytes, p[i])
-			emojiStr := emojiMap[string(lockBytes)]
+			emojiBytes = append(emojiBytes, p[i])
+			emojiStr := emojiMap[string(emojiBytes)]
 			if emojiStr != "" {
-				emojiBytes = append(emojiBytes, []byte(emojiStr)...)
+				waitForWriteBytes = append(waitForWriteBytes, []byte(emojiStr)...)
 			} else {
-				emojiBytes = append(emojiBytes, lockBytes...)
+				waitForWriteBytes = append(waitForWriteBytes, emojiBytes...)
 			}
-			lockBytes = []byte{}
+			emojiBytes = []byte{}
 			continue
 		}
-		lockBytes = append(lockBytes, p[i])
+		emojiBytes = append(emojiBytes, p[i])
 		if p[i] == 32 {
 			lock = false
-			emojiBytes = append(emojiBytes, lockBytes...)
-			lockBytes = []byte{}
+			waitForWriteBytes = append(waitForWriteBytes, emojiBytes...)
+			emojiBytes = []byte{}
 		}
 	}
-	*pPtr = emojiBytes
-	_, err = emojiHookTramp(fd, p)
-	return pLen, err
+	*pPtr = waitForWriteBytes
+	wLen := len(waitForWriteBytes)
+	w, err := emojiHookTramp(fd, p)
+	if w == wLen {
+		return pLen, err
+	} else {
+		return w, err
+	}
 }
 
 func emojiHookTramp(fd int, p []byte) (n int, err error) {
